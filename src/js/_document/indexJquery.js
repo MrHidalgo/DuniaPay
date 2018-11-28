ad7873633a4e1bd47e5b56dc3df0bc1b771c5788
MrHidalgo <Luckyhidalgo@gmail.com>
2018-11-28 13:51:52 +0200

    Create transfer box - w\ adaptive.

--- src/js/_document/indexJquery.js
+++ src/js/_document/indexJquery.js
@@ -21,10 +21,12 @@ $(document).ready((ev) => {
    *
    */
   $('body').on('click', (ev) => {
-    const className = ".header__profile";
+    const className = ".header__profile, .transfer__box-drop";
 
     if (!$(ev.target).closest(className).length) {
       $('.header__profile-drop').fadeOut(300);
+      $('.transfer__box-dropdown').slideUp(300);
+
     }
   });
 
@@ -77,6 +79,32 @@ $(document).ready((ev) => {
       $(ev.currentTarget).find('.header__profile-drop').fadeToggle(300);
     });
   };
+
+
+  /**
+   *
+   */
+  const initTransferDropDown = () => {
+    $('[transfer-btn-js]').on('click', (ev) => {
+      const elem = $(ev.currentTarget),
+        parentElem = elem.closest('.transfer__box-drop');
+
+      $('.transfer__box-dropdown').slideUp(300);
+      parentElem.find('.transfer__box-dropdown').slideToggle(300);
+    });
+    $('.transfer__box-dropdown a').on('click', (ev) => {
+      const elem = $(ev.currentTarget),
+        parentElem = elem.closest('.transfer__box-drop'),
+        insertNode = parentElem.find('[transfer-btn-js]'),
+        currencyName = elem.find('p').text(),
+        imgSrc = elem.find('img').attr('src');
+
+      insertNode.find('p').html(currencyName);
+      insertNode.find('img').attr('src', imgSrc);
+
+      parentElem.find('.transfer__box-dropdown').slideToggle(300);
+    });
+  };
   /*
   * CALLBACK :: end
   * ============================================= */
@@ -101,6 +129,7 @@ $(document).ready((ev) => {
     initAsideMenu();
     initSidebarBtn();
     initHeaderProfileDropDown();
+    initTransferDropDown();
 		// ==========================================
   };
   initJquery();
